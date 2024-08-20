4b42081062bbe22f3e4fc010ce676a0084d2d2cb
Dewmo123 <akh200808@gmail.com>
2024-08-21 00:50:15 +0900

    .fix weather

--- Assets/Work/AKH/Scripts/Core/WeatherManager.cs
+++ Assets/Work/AKH/Scripts/Core/WeatherManager.cs
@@ -2,18 +2,66 @@ using SerializableDictionary.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class WeatherManager : MonoSingleton<WeatherManager>
 {
-    public List<WeatherSO> weathers;
-    public Queue<WeatherSO> weatherQueue;
+    private float _currentTime;
+    private Player _player;
+    [SerializeField] private float _hitTime;
+    [SerializeField] private WeatherUI _coreWeather;
+    private WeatherSO _curWeather;
+
+    [field:SerializeField]public List<WeatherSO> weathers { get; private set; }
+    public List<WeatherSO> curWeathers;
+    private int cnt = -1;
     private void Awake()
     {
-        weatherQueue = new Queue<WeatherSO>();
-        for(int i = 1; i <= 57; i++)
+        SetWeathers();
+        StartCoroutine(FindPlayer());
+    }
+
+    private IEnumerator FindPlayer()
+    {
+        yield return new WaitUntil(() => GameManager.instance.Player!=null);
+        _player = GameManager.instance.Player;
+    }
+
+    private void Update()
+    {
+        if (_currentTime >= _hitTime && _player != null)
+        {
+            _curWeather = _coreWeather.curWeather.Value;
+            _currentTime = 0;
+            _player.healthCompo.ChangeValue(_curWeather.healthPerSec);
+            _player.hungryCompo.ChangeValue(_curWeather.hungryPerSec);
+            _player.waterCompo.ChangeValue(_curWeather.waterPerSec);
+        }
+        _currentTime += Time.deltaTime;
+    }
+    private void SetWeathers()
+    {
+        curWeathers = new List<WeatherSO>();
+        for (int i = 1; i <= 57; i++)
         {
             WeatherSO weather = weathers[Random.Range(0, weathers.Count)];
-            weatherQueue.Enqueue(weather);
+            curWeathers.Add(weather);
         }
+    }
+    public List<WeatherSO> GetWeekWeather()
+    {
+        List<WeatherSO> weekWeather = new List<WeatherSO>();
+        for(int i = 1; i <= 7; i++)
+        {
+            weekWeather.Add(weathers[i+cnt]);
+        }
+        return weekWeather;
+    }
+    public WeatherSO GetNextWeather()
+    {
+        if (cnt < curWeathers.Count)
+            cnt++;
+        else cnt = -1;
+        return weathers[cnt];
     }
 }
--- Assets/Work/AKH/Scripts/UI/WeatherUI.cs
+++ Assets/Work/AKH/Scripts/UI/WeatherUI.cs
@@ -8,20 +8,19 @@ using UnityEngine.UI;
 
 public class WeatherUI : PlayerConnectUI
 {
-    private NotifyValue<WeatherSO> _curWeather;
-    private float _currentTime;
-    [SerializeField] private float _hitTime;
+    public NotifyValue<WeatherSO> curWeather { get; private set; }
+
     [SerializeField] private Image _weatherImage;
     [SerializeField] private TextMeshProUGUI _weatherTxt;
     private void Awake()
     {
-        _curWeather = new NotifyValue<WeatherSO>();
-        _curWeather.OnvalueChanged += HandleWeatherChanged;
+        curWeather = new NotifyValue<WeatherSO>();
+        curWeather.OnvalueChanged += HandleWeatherChanged;
     }
     protected override void Start()
     {
         base.Start();
-        _curWeather.Value = WeatherManager.instance.weatherQueue.Dequeue();
+        curWeather.Value = WeatherManager.instance.GetNextWeather();
     }
     private void HandleWeatherChanged(WeatherSO prev, WeatherSO next)
     {
@@ -29,21 +28,6 @@ public class WeatherUI : PlayerConnectUI
         _weatherTxt.text = next.weatherName;
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            _curWeather.Value = WeatherManager.instance.weatherQueue.Dequeue();
-        }
-        if (_currentTime >= _hitTime&&_player!=null)
-        {
-            _currentTime = 0;
-            _player.healthCompo.ChangeValue(_curWeather.Value.healthPerSec);
-            _player.hungryCompo.ChangeValue(_curWeather.Value.hungryPerSec);
-            _player.waterCompo.ChangeValue(_curWeather.Value.waterPerSec);
-        }
-        _currentTime += Time.deltaTime;
-    }
     public override void AfterFindPlayer()
     {
     }
