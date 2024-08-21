9bfedb4e928815837a867d0583c72fd8c1fb8ea3
machany <machanicaly@gmail.com>
2024-08-22 00:34:32 +0900

    Reapply "perf : Interaction"
    This reverts commit e35771e12fb046151827624db0659a6a251816db.

--- Assets/Work/YHB/03_So/01_Scripts/InteractionObjectInfoSo.cs
+++ Assets/Work/YHB/03_So/01_Scripts/InteractionObjectInfoSo.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SO/InteractionObjectString")]
 public class InteractionObjectInfoSo : ScriptableObject
 {
-    [Header("Setting")]
-    [Tooltip("딕셔너리의 키로 들어갈 이름입니다.")]
-    public string _code;
+    [HideInInspector] public string _code;
 
     [Header("FKeySet")]
     [Tooltip("F키를 띄울지 여부입니다.")]
@@ -20,12 +18,8 @@ public class InteractionObjectInfoSo : ScriptableObject
     [Header("BigTitleSet")]
     [Tooltip("플레이어에게 표시딜 때 크게 표시 되는 지 여부입니다.")]
     public bool _bigTitle;
-    [Tooltip("다시 활성화 될때 까지의 시간")]
-    public float _activationTime;
 
     [Header("String")]
     [Tooltip("게임에 표시될 문장입니다.")]
     public string _str;
-
-    public bool _canActivation = true;
 }
