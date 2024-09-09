ef237a0e2d7ae595c34334d645bea1a8b601ae90
Dewmo123 <akh200808@gmail.com>
2024-09-09 23:59:16 +0900

    .fix difficult

--- Assets/Work/AKH/Scripts/UI/InteractionUI/TotemUI.cs
+++ Assets/Work/AKH/Scripts/UI/InteractionUI/TotemUI.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TotemUI : InteractionUI
-{
-    private void Start()
-    {
+public class TotemUI : InteractionUI {
+    private void Start() {
         AddDic();
     }
-    public override void AddDic()
-    {
-        InteractionManager.instance.InteractionUIDic.Add(MyType,this);
+    public void ShowQuestion(DiffucultEnum type) {
+        QuestionUI question = InteractionManager.instance.InteractionUIDic[UIType.Problem] as QuestionUI;
+        question.Set(type);
+        question.IncreaseCnt();
+    }
+    public override void AddDic() {
+        InteractionManager.instance.InteractionUIDic.Add(MyType, this);
     }
 }
