77e7c22150979d240f6aa84672d6a02908814a32
Dewmo123 <akh200808@gmail.com>
2024-09-18 20:02:50 +0900

    .complete TableUI

--- Assets/Work/AKH/Scripts/UI/HouseInfoUI.cs
+++ Assets/Work/AKH/Scripts/UI/HouseInfoUI.cs
@@ -9,8 +9,8 @@ public class HouseInfoUI : GoodsInfoUI
     [SerializeField] private HouseSO _goods;
     private void Start()
     {
-        _price.text = $"나무: {_goods.woodCount}개, 돌: {_goods.rockCount}";
-        _goodsInfo.text = _goods.info;
+        _price.text = $"나무: {_goods.woodCount}개, 돌: {_goods.rockCount}개";
+        _goodsInfo.text = _goods.houseName+"\n"+_goods.info;
         _image.sprite = _goods.houseImage;
     }
 }
